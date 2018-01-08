8b71fb93fe09d2d9ef8a226165c69bc9fcd326a7
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-08 13:34:02 -0600

    Added routing between the home page and the event details page.

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,9 +7,14 @@ import {NavBarComponent} from "./nav/navbar.component";
 import {EventService} from "./events/shared/event.service";
 import {ToastrService} from "./common/toastr.service";
 import {EventDetailsComponent} from "./events/event-details/event-details.component";
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./routes";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
