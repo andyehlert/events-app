0685ea0293a5c78b7ffd842685dbea44b972530b
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-08 16:07:27 -0600

    Created an event model to provide type safety.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from "@angular/core";
-import {EventService} from "./shared/event.service";
-import {ToastrService} from "../common/toastr.service";
 import {ActivatedRoute} from "@angular/router";
+import {ToastrService} from "../common/toastr.service";
+import {IEvent, EventService} from "./shared/index";
 
 @Component({
     template: `
@@ -17,7 +17,7 @@ import {ActivatedRoute} from "@angular/router";
     `
 })
 export class EventsListComponent implements OnInit {
-    events:any;
+    events:IEvent[];
 
     constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {}
 
