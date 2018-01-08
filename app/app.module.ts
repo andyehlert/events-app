dbfbdf8ab93445c2be749ed58cbcdffacf3a152a
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-08 12:54:41 -0600

    Created a new component for displaying an event details page.

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,7 @@ import {EventThumbnailComponent} from "./events/event-thumbnail.component";
 import {NavBarComponent} from "./nav/navbar.component";
 import {EventService} from "./events/shared/event.service";
 import {ToastrService} from "./common/toastr.service";
+import {EventDetailsComponent} from "./events/event-details/event-details.component";
 
 @NgModule({
     imports: [BrowserModule],
@@ -13,6 +14,7 @@ import {ToastrService} from "./common/toastr.service";
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
+        EventDetailsComponent,
         NavBarComponent
     ],
     providers: [
