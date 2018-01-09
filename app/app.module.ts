fcd47ce463447900f2fb52ca94989139ed1bee80
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-09 16:02:29 -0600

    Added a reusable collapsible well component and altered the session list component to used it.

--- app/app.module.ts
+++ app/app.module.ts
@@ -19,6 +19,7 @@ import {
 } from './events/index';
 import {AuthService} from "./user/auth.service";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CollapsibleWellComponent} from "./common/collapsible-well.component";
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
         CreateEventComponent,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponent
+        SessionListComponent,
+        CollapsibleWellComponent
     ],
     providers: [
         EventService,
