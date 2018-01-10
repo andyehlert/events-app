f96eb5b5c672c7505c33e8b1a8a27801cf423459
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-10 15:00:55 -0600

    Changed the toastr service to be injected using the InjectToken class. Also removed the toastr success message from the events-list component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {EventsAppComponent} from "./events-app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {NavBarComponent} from "./nav/navbar.component";
-import {ToastrService} from "./common/toastr.service";
+import {TOASTR_TOKEN, Toastr} from "./common/toastr.service";
 import {RouterModule} from "@angular/router";
 import {appRoutes} from "./routes";
 import {Error404Component} from "./errors/404.component";
@@ -22,6 +22,8 @@ import {AuthService} from "./user/auth.service";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CollapsibleWellComponent} from "./common/collapsible-well.component";
 
+// Lets TypeScript know that this is a known variable
+declare let toastr:Toastr;
 
 @NgModule({
     imports: [
@@ -45,7 +47,7 @@ import {CollapsibleWellComponent} from "./common/collapsible-well.component";
     ],
     providers: [
         EventService,
-        ToastrService,
+        {provide: TOASTR_TOKEN, useValue: toastr},
         EventRouteActivator,
         EventsListResolver,
         AuthService,
