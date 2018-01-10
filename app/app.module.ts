aaf5210d661c0e2b860a1ad5c2136033fb11fab4
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-10 16:01:37 -0600

    Created a jQuery wrapper service.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,17 @@ import {NgModule} from "@angular/core";
 import {EventsAppComponent} from "./events-app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {NavBarComponent} from "./nav/navbar.component";
-import {TOASTR_TOKEN, Toastr} from "./common/toastr.service";
 import {RouterModule} from "@angular/router";
 import {appRoutes} from "./routes";
 import {Error404Component} from "./errors/404.component";
+import {AuthService} from "./user/auth.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {
+    JQ_TOKEN,
+    TOASTR_TOKEN,
+    Toastr,
+    CollapsibleWellComponent
+} from "./common/index";
 import {
     CreateEventComponent,
     CreateSessionComponent,
@@ -18,12 +25,10 @@ import {
     SessionListComponent,
     DurationPipe
 } from './events/index';
-import {AuthService} from "./user/auth.service";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CollapsibleWellComponent} from "./common/collapsible-well.component";
 
 // Lets TypeScript know that this is a known variable
 declare let toastr:Toastr;
+declare let jQuery:Object;
 
 @NgModule({
     imports: [
@@ -48,6 +53,7 @@ declare let toastr:Toastr;
     providers: [
         EventService,
         {provide: TOASTR_TOKEN, useValue: toastr},
+        {provide: JQ_TOKEN, useValue: jQuery},
         EventRouteActivator,
         EventsListResolver,
         AuthService,
