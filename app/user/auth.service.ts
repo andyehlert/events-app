1db2fc2834b94a0d3ff9fca8d1263a59b8949712
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-11 20:09:45 -0600

    Now validating login against profiles on the local server.

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,17 +1,26 @@
 import {Injectable} from "@angular/core";
 import {IUser} from "./user.model";
+import {Http, Headers, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class AuthService {
     currentUser:IUser;
 
+    constructor(private http:Http) {}
+
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'John',
-            lastName: 'Doe'
-        }
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let loginInfo = {username: userName, password: password};
+
+        return this.http.post('/api/login', JSON.stringify(loginInfo), options).do(response => {
+            if (response) {
+                this.currentUser = <IUser>response.json().user;
+            }
+        }).catch(error => {
+            return Observable.of(false);
+        });
     }
 
     isAuthenticated() {
