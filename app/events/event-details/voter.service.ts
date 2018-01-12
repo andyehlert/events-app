a2b837f0842caa1c3df5223a12968e8abd953582
Andy Ehlert <aehlert@Andys-MacBook-Pro.local>
2018-01-11 19:54:47 -0600

    Now using http requests to store voter data on the local server.

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,17 +1,34 @@
 import {Injectable} from "@angular/core";
-import {ISession} from "../shared/event.model";
+import {IEvent, ISession} from "../shared/event.model";
+import {Headers, Http, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class VoterService {
-    addVoter(session:ISession, voterName:string) {
+    constructor(private http:Http) {}
+
+    addVoter(eventId:number, session:ISession, voterName:string) {
         session.voters.push(voterName);
+
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        this.http.post(url, JSON.stringify({}), options).catch(this.handleError).subscribe();
     }
 
-    deleteVoter(session:ISession, voterName:string) {
+    deleteVoter(eventId:number, session:ISession, voterName:string) {
         session.voters = session.voters.filter(voter => voter !== voterName);
+
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        this.http.delete(url).catch(this.handleError).subscribe();
     }
 
     userHasVoted(session:ISession, voterName:string) {
         return session.voters.some(voter => voter === voterName);
+    }
+
+    private handleError(error) {
+        return Observable.throw(error.statusText);
     }
 }
